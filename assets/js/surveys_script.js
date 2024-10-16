56dccf3745b9797015138d8c19dfe2152f71f24f
markalterego <122254234+markalterego@users.noreply.github.com>
2024-10-16 20:22:32 +0300

    tables update according to user input

--- assets/js/surveys_script.js
+++ assets/js/surveys_script.js
@@ -7,47 +7,54 @@ const icecreamFormAnswer = document.getElementById('icecreamFormAnswer');
 
 const answeredTable = document.getElementById('answered'); 
 
-let answered = [];
+const answered = // holds the count of all survey answers
+[
+    { answer: 'dogs', count: 0 },
+    { answer: 'cats', count: 0 },
+    { answer: 'other', count: 0 },
+    { answer: 'vanilla', count: 0 },
+    { answer: 'chocolate', count: 0 },
+    { answer: 'strawberry', count: 0 },
+    { answer: 'other2', count: 0 },
+    { answer: '???', count: 0 }
+]; 
 
 animalForm.addEventListener('submit', (e) => // when form is submitted
 {
     e.preventDefault();
     
-    const formData = new FormData(animalForm); // [0]name[1]item
-    
-    for(item of formData)
-    {
-        if(item[1]=='dogs')
-        {
-            animalFormAnswer.textContent = 'You answered: Dogs! Woof!';
-        }
-        else if(item[1]=='cats')
-        {
-            animalFormAnswer.textContent = 'You answered: Cats! Meow!';
-        }
-        else if(item[1]=='other')
-        {
-            animalFormAnswer.textContent = 'You answered: Other! * makes unknown sound *';
-        }
+    const formData = new FormData(animalForm); 
 
-        answered.push(item);
+    let item = formData.get('animalQuestion'); // finding value for key input
+
+    if(item==='dogs')
+    {
+        animalFormAnswer.textContent = 'You answered: Dogs! Woof!';
+    }
+    else if(item==='cats')
+    {
+        animalFormAnswer.textContent = 'You answered: Cats! Meow!';
+    }
+    else if(item==='other')
+    {
+        animalFormAnswer.textContent = 'You answered: Other! * makes unknown sound *';
+    }
+    else
+    {
+        animalFormAnswer.textContent = 'You answered: Nothing! Not a fan of animals?';
     }
 
     for(let i = 0; i < answered.length; i++)
-    {   
-        if(i == 0)
+    {
+        if(item === answered[i].answer)
         {
-            answeredTable.textContent = '';
-        }    
-            
-        const tr = document.createElement('tr'); // create row
-        const td = document.createElement('td'); // create cell
-
-        td.textContent = answered[i][1];
-
-        tr.appendChild(td);
-        answeredTable.appendChild(tr);
+            answered[i].count++; // updating the count of the given answer
+            break;
+        }
     }
+
+    console.log(answered);
+    updateTable();
 });
 
 icecreamForm.addEventListener('submit', (e) => 
@@ -58,32 +65,36 @@ icecreamForm.addEventListener('submit', (e) =>
     const p = document.createElement('p');
 
     let ans = 'You answered: ';
+    let item = '';
+    let items = [];
 
     for(item of formData)
     {   
-        if(item[1]=='vanilla')
+        if(item[1]==='vanilla')
         {
-            ans += 'Vanilla, ';
+            ans += 'Vanilla, '; 
         }
-        else if(item[1]=='chocolate')
+        else if(item[1]==='chocolate')
         {
             ans += 'Chocolate, ';
         }
-        else if(item[1]=='strawberry')
+        else if(item[1]==='strawberry')
         {
             ans += 'Strawberry, ';
         }
-        else if(item[1]=='other2')
+        else if(item[1]==='other2')
         {
             ans += 'Other, ';
         }
+
+        items.push(item[1]);
     }
 
-    if(ans == 'You answered: ')
+    if(ans === 'You answered: ')
     {
         ans += 'Nothing! I need to update my list of options...';
     }
-    else if(ans == 'You answered: Other, ')
+    else if(ans === 'You answered: Other, ')
     {
         ans = ans.slice(0, -2); // get rid of space after last item
         ans += '! Wonder what flavor that would be...';
@@ -95,14 +106,52 @@ icecreamForm.addEventListener('submit', (e) =>
     }
 
     icecreamFormAnswer.textContent = ans;
+
+    let i = 0;
+    let ii = 0;
+
+    while(i < items.length) // iterating survey answers
+    {
+        while(ii < answered.length) // iterating possible answers
+        {
+            if(items[i] === answered[ii].answer)
+            {
+                answered[ii].count++; // updating the count of the given answer
+            }
+            ii++;
+        }
+        ii=0, i++;
+    }
+
+    console.log(answered);
+    updateTable();
 });
 
 templateForm.addEventListener('submit', (e) =>
 {
     e.preventDefault();
+    
+    let lastindex = answered.pop();
+    lastindex.count++;
+    answered.push(lastindex);
+
+    updateTable();
 
     alert('\nALERT!!\n\n' +
           '1. This button is under maintenance\n' +
           '2. Please refrain from touching\n' +
           '3. Thank you for understanding');
 });
+
+function updateTable()
+{
+    for(let i = 0; i < answered.length; i++)
+    {   
+        let s = 'cell_' + answered[i].answer;
+        
+        if(document.getElementById(s) !== null) // searching for a table cell
+        {
+            document.getElementById(s).textContent = answered[i].count;
+        }
+    }
+}
